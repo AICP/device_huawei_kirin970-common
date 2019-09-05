583781532e48544d2edab4b28bfc977f67a95bf5
Han Wang <416810799@qq.com>
2019-09-05 07:07:51 +0200

    kirin970-common: Remove LOCAL_MODULE_TAGS := eng usage
    Change-Id: I83aa494618d9c4809d3219aa3785e56b48ef8dfb

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,7 +2,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.kirin970.rc
-LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.kirin970.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
